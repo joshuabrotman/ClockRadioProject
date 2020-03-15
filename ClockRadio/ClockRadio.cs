e85e382b159dbdab929e15450a8316afdc13e9d4
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 19:42:15 -0500

    message

---
+++ ClockRadio/ClockRadio.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class ClockRadio
+    {
+        public string currentTime;
+        public string currentRadioStation;
+        public bool alarmState;
+        public string alarmTime;
+
+        public ClockRadio()
+        {
+            currentTime = "19:00";
+            currentRadioStation = "97 QM";
+            alarmState = false;
+            alarmTime = "06:00";
+        }
+
+        public string GetTime()
+        {
+            return currentTime;
+        }
+
+        public void SetTime(string newTime)
+        {
+            currentTime = newTime;
+        }
+
+        public string GetStation()
+        {
+            return currentRadioStation;
+        }
+
+        public void SetStation(string newStation)
+        {
+            currentRadioStation = newStation;
+        }
+
+        public string GetAlarmTime()
+        {
+            return alarmTime;
+        }
+
+        public void SetAlarmTime(string newAlarmTime)
+        {
+            alarmTime = newAlarmTime;
+        }
+
+        public void ToggleAlarm()
+        {
+            if (alarmState == true)
+            {
+                alarmState = false;
+            }
+            else
+            {
+                alarmState = true;
+            }
+        }
+    }
+}
---
+++ ClockRadio/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockRadio MyClock = new ClockRadio();
+            bool exitConsole = false;
+            while (exitConsole != true)
+            {
+
+             
+                Console.WriteLine("The current time is: " + MyClock.GetTime());
+                Console.WriteLine("The current station is set to: " + MyClock.GetStation());
+                Console.WriteLine("The alarm is set to: " + MyClock.GetAlarmTime());
+
+                Console.WriteLine("Enter T - update time, S - update station, A - set Alarm, H - toggle Alarm, Q - Quit:");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "T":
+                        Console.WriteLine("Enter a new Time: ");
+                        userInput = Console.ReadLine();
+                        MyClock.SetTime(userInput);
+                        break;
+                    case "S":
+                        Console.WriteLine("Enter a new Station");
+                        userInput = Console.ReadLine();
+                        MyClock.SetStation(userInput);
+                        break;
+                    case "A":
+                        Console.WriteLine("Enter a new alarm time: ");
+                        userInput = Console.ReadLine();
+                        MyClock.SetAlarmTime(userInput);
+                        break;
+                    case "H":
+                        MyClock.ToggleAlarm();
+                        break;
+                    case "Q":
+                        exitConsole = true;
+                        break;
+                }
+
+            }
+        }
+    }
+
+   
+}
